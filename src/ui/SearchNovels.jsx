bec1d6ebc5cfd316757643375ef4357d28e214c4
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2024-06-14 03:15:44 +0700

    style: add search icon

--- src/ui/SearchNovels.jsx
+++ src/ui/SearchNovels.jsx
@@ -1,9 +1,12 @@
 function SearchNovels() {
   return (
-    <input
-      placeholder="Search novels..."
-      className="w-64 rounded-full bg-sky-100 px-4 py-2 text-sm text-gray-900 transition-all duration-300 placeholder:text-stone-400 focus:w-72 focus:outline-none focus:ring focus:ring-sky-500 focus:ring-opacity-50"
-    />
+    <>
+      <input
+        placeholder="Search novels..."
+        className="w-64 rounded-full bg-sky-100 px-4 py-2 text-sm text-gray-900 transition-all duration-300 placeholder:text-stone-400 focus:w-72 focus:outline-none focus:ring focus:ring-sky-500 focus:ring-opacity-50"
+      />
+      <ion-icon class="search-icon" name="search-outline"></ion-icon>
+    </>
   );
 }
 
