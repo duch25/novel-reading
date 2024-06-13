fde2ff9a4c3b58e081c62fce47b4464288b2bf45
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2024-06-14 03:17:45 +0700

    fix: fix types in logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <Link to="/" className="tracking-wides text-lg font-semibold uppercase">
-      Ptruyen
+    <Link to="/" className="tracking-wides text-xl font-semibold uppercase">
+      PTruyen
     </Link>
   );
 }
