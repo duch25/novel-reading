831d6d652afe9d5506719ef28b8d432fb0bd028d
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2024-06-15 20:38:50 +0700

    feat: implement feature download

---
+++ src/features/download/DownloadBox.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react';
+import SpinnerMini from '../../ui/SpinnerMini';
+import { useParams } from 'react-router-dom';
+import { getAllFileFormats, download } from '../../services/apiDownload';
+
+function DownloadBox() {
+  const [types, setTypes] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { id, chapter } = useParams();
+
+  const handleDownLoad = (data, fileName, type) => {
+    const byteCharacters = atob(data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+
+    const blob = new Blob([byteArray], { type: `application/${type}` });
+
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+
+  const handleFetchDownloadRequest = async type => {
+    const requestBody = {
+      novel_id: id,
+      chapter_id: chapter,
+      type: type.toLowerCase(),
+      domain: 'truyen.tangthuvien.vn',
+    };
+
+    const data = await download(requestBody);
+    handleDownLoad(data.bytes, data.filename, data.type);
+    // console.log(data);
+  };
+
+  useEffect(function () {
+    async function getAllTypes() {
+      setIsLoading(true);
+
+      const data = await getAllFileFormats();
+      setTypes(data);
+
+      setIsLoading(false);
+    }
+
+    getAllTypes();
+  }, []);
+
+  return (
+    <div className="absolute flex h-24 w-40 flex-col items-center justify-between rounded-lg bg-sky-950 px-3 py-2 text-white shadow-lg hover:bg-sky-950 hover:text-white">
+      {isLoading ? (
+        <SpinnerMini />
+      ) : (
+        <>
+          <h3 className="text-medium">Định dạng</h3>
+          <div className="flex gap-4">
+            {types?.map(type => (
+              <div
+                key={type}
+                className={`rounded-md shadow-lg ${type === 'PDF' ? 'bg-rose-700' : 'bg-sky-700'}`}
+              >
+                <label
+                  className="cursor-pointer px-3 py-2"
+                  htmlFor={type.toLowerCase()}
+                >
+                  .{type.toLowerCase()}
+                </label>
+                <input
+                  type="radio"
+                  className="hidden"
+                  id={type.toLowerCase()}
+                  name="format"
+                  value={type.toLowerCase()}
+                  onClick={e => handleFetchDownloadRequest(e.target.value)}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default DownloadBox;
