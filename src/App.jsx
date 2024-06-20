1f48c3bd7063acab6603f8ffccfd7b032f1b1552
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2024-06-20 23:18:16 +0700

    update: add some router

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,30 @@ import Homepage from './pages/Homepage';
 import NovelDetails from './pages/NovelDetails';
 import NovelReading from './pages/NovelReading';
 import PageNotFound from './pages/PageNotFound';
-import NovelsByGenre from './pages/NovelsByGenre';
 import ReadingHistory from './pages/ReadingHistory';
 import AppLayout from './ui/AppLayout';
+import { NavigateItemsProvider } from './context/NavigateItemContext';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="genres/:id" element={<NovelsByGenre />} />
-            <Route path="novels/:id" element={<NovelDetails />} />
-            <Route path="reading/:id" element={<NovelReading />} />
-            <Route path="reading-history" element={<ReadingHistory />} />
-          </Route>
+      <NavigateItemsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="novels" element={<Homepage />} />
+              <Route path="novels/:id" element={<NovelDetails />} />
+              <Route path="novels/search" element={<SearchResults />} />
+              <Route path="reading/:id/:chapter" element={<NovelReading />} />
+              <Route path="reading-history" element={<ReadingHistory />} />
+            </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </NavigateItemsProvider>
     </>
   );
 }
